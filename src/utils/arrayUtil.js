bc0b0ae4dff0a22b1c16dc639e668e659dc46173
Martin Marianetti <martin.marianetti@gmail.com>
2017-04-03 21:20:24 -0300

    added new functions

--- src/utils/arrayUtil.js
+++ src/utils/arrayUtil.js
@@ -14,6 +14,18 @@ class ArrayUtil {
     return (arrhaystack.indexOf(needle) > -1);
   }
 
+  static isEmpty(arr){
+    return arr.length === 0;
+  }
+
+  static remove(element, arr) {
+    let index = arr.indexOf(element);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
 }
 
 
--- src/utils/stringUtil.js
+++ src/utils/stringUtil.js
@@ -5,6 +5,30 @@ class StringUtil {
 
     constructor () {}
 
+    static isEmpty(str){
+      return str.length === 0;
+    }
+
+    static isBlank(str){
+      return str.trim().length === 0;
+    }
+
+    static equals(str1, str2){
+        return str1 === str2;
+    }
+
+    static contains(str, str2) {
+        return str.indexOf( str2 ) !== -1;
+    }
+
+    static truncate(str, length){
+        return str.substring(0,length);
+    }
+
+    static capitalizeFirstLetter(string) {
+        return string[0].toUpperCase() + string.slice(1).toLowerCase();
+    }
+
     static replaceAll(str, find, replace) {
 
         if(!replace) {
